3c2e5d29c7a7b66842f6d5240acd09b452d33e83
Sebastien Gruchet <gruchet@gmail.com>
2026-03-20 06:47:59 +0100

    More UI improvements

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '10f2e6f018f0b0c7ad2e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '06deaf99540b390e5bc2');
